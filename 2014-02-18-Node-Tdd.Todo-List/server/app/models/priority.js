b4efa9ac008f55a30bb11d84189b62b9c65b2913
Chyld Medford <chyld.medford@gmail.com>
2014-02-18 20:09:48 -0600

    todo 3

--- 2014-02-18-Node-Tdd.Todo-List/server/app/models/priority.js
+++ 2014-02-18-Node-Tdd.Todo-List/server/app/models/priority.js
@@ -2,15 +2,44 @@
 
 module.exports = Priority;
 var priorities = global.nss.db.collection('priorities');
+var Mongo = require('mongodb');
 
 function Priority(priority){
+  this._id = priority._id;
   this.name = priority.name;
   this.value = parseInt(priority.value);
 }
 
 Priority.prototype.save = function(fn){
-  priorities.save(this, function(err, record){
-    fn(record);
+  var self = this;
+
+  Priority.findByName(this.name, function(priority){
+    if(!priority){
+      priorities.save(self, function(err, record){
+        fn(err);
+      });
+    }else{
+      fn(new Error('Duplicate Priority'));
+    }
+  });
+};
+
+Priority.findAll = function(fn){
+  priorities.find().toArray(function(err, records){
+    fn(records);
+  });
+};
+
+Priority.findByName = function(name, fn){
+  priorities.findOne({name:name}, function(err, record){
+    fn(record ? new Priority(record) : null);
+  });
+};
+
+Priority.findById = function(id, fn){
+  var _id = Mongo.ObjectID(id);
+  priorities.findOne({_id:_id}, function(err, record){
+    fn(record ? new Priority(record) : null);
   });
 };
 
--- 2014-02-18-Node-Tdd.Todo-List/server/test/unit/priority.js
+++ 2014-02-18-Node-Tdd.Todo-List/server/test/unit/priority.js
@@ -15,6 +15,12 @@ describe('Priority', function(){
     });
   });
 
+  beforeEach(function(done){
+    global.nss.db.dropDatabase(function(err, result){
+      done();
+    });
+  });
+
   describe('new', function(){
     it('should create a new Priority', function(){
       var obj = {name:'High', value: '10'};
@@ -30,9 +36,102 @@ describe('Priority', function(){
     it('should save a Priority object into the database', function(done){
       var obj = {name:'High', value: '10'};
       var p1 = new Priority(obj);
-      p1.save(function(savedPriority){
-        expect(savedPriority).to.be.instanceof(Priority);
-        expect(savedPriority).to.have.property('_id').and.be.ok;
+      p1.save(function(err){
+        expect(err).to.be.null;
+        expect(p1).to.be.instanceof(Priority);
+        expect(p1.name).to.equal('High');
+        expect(p1.value).to.deep.equal(10);
+        expect(p1).to.have.property('_id').and.be.ok;
+        done();
+      });
+    });
+
+    it('should not create duplicate priorites based on name', function(done){
+      var p1 = new Priority({name:'High', value:'10'});
+      var p2 = new Priority({name:'Medium', value:'5'});
+      var p3 = new Priority({name:'High', value:'1'});
+
+      p1.save(function(){
+        p2.save(function(){
+          p3.save(function(err){
+            expect(err).to.be.instanceof(Error);
+            expect(p3._id).to.be.undefined;
+            done();
+          });
+        });
+      });
+    });
+  });
+
+  describe('.findAll', function(){
+    it('should return all Priorities in the datbase', function(done){
+      var p1 = new Priority({name:'High', value:'10'});
+      var p2 = new Priority({name:'Medium', value:'5'});
+      var p3 = new Priority({name:'Low', value:'1'});
+
+      p1.save(function(){
+        p2.save(function(){
+          p3.save(function(){
+            Priority.findAll(function(priorities){
+              expect(priorities).to.have.length(3);
+              done();
+            });
+          });
+        });
+      });
+    });
+  });
+
+  describe('.findByName', function(){
+    it('should find the Priority by its name', function(done){
+      var p1 = new Priority({name:'High', value:'10'});
+      var p2 = new Priority({name:'Medium', value:'5'});
+      var p3 = new Priority({name:'Low', value:'1'});
+
+      p1.save(function(){
+        p2.save(function(){
+          p3.save(function(){
+            Priority.findByName('Medium', function(foundPriority){
+              expect(foundPriority).to.be.instanceof(Priority);
+              expect(foundPriority.name).to.equal('Medium');
+              done();
+            });
+          });
+        });
+      });
+    });
+
+    it('should not find the Priority by its name', function(done){
+      Priority.findByName('Medium', function(foundPriority){
+        expect(foundPriority).to.be.null;
+        done();
+      });
+    });
+  });
+
+  describe('.findById', function(){
+    it('should find the Priority by its id', function(done){
+      var p1 = new Priority({name:'High', value:'10'});
+      var p2 = new Priority({name:'Medium', value:'5'});
+      var p3 = new Priority({name:'Low', value:'1'});
+
+      p1.save(function(){
+        p2.save(function(){
+          p3.save(function(){
+            var id = p2._id.toString();
+            Priority.findById(id, function(foundPriority){
+              expect(foundPriority).to.be.instanceof(Priority);
+              expect(foundPriority._id.toString()).to.equal(id);
+              done();
+            });
+          });
+        });
+      });
+    });
+
+    it('should not find the Priority by its id', function(done){
+      Priority.findById('01234567890123456789abcd', function(foundPriority){
+        expect(foundPriority).to.be.null;
         done();
       });
     });
