78f413eaa304dd1ca0fc42ba66b1230b3e44a34b
Chyld Medford <chyld.medford@gmail.com>
2014-02-27 18:42:48 -0600

    photo album final

--- 2014-02-26-File-Uploads.PhotoAlbum/app/routes/albums.js
+++ 2014-02-26-File-Uploads.PhotoAlbum/app/routes/albums.js
@@ -1,6 +1,19 @@
 'use strict';
 
 var Album = require('../models/album');
+var moment = require('moment');
+
+exports.index = function(req, res){
+  Album.findAll(function(albums){
+    res.render('albums/index', {moment:moment, albums:albums, title: 'Photo Albums'});
+  });
+};
+
+exports.show = function(req, res){
+  Album.findById(req.params.id, function(album){
+    res.render('albums/show', {moment:moment, album:album, title: album.title});
+  });
+};
 
 exports.new = function(req, res){
   res.render('albums/new', {title: 'New Album'});
@@ -11,6 +24,15 @@ exports.create = function(req, res){
   album.addCover(req.files.cover.path);
   album.insert(function(){
     res.redirect('/');
+  });
+};
+
+exports.photoAdd = function(req, res){
+  Album.findById(req.params.id, function(album){
+    album.addPhoto(req.files.photo.path, req.files.photo.name);
+    album.update(function(){
+      res.redirect('/albums/' + req.params.id);
+    });
   });
 };
 
--- 2014-02-26-File-Uploads.PhotoAlbum/test/acceptance/albums_spec.js
+++ 2014-02-26-File-Uploads.PhotoAlbum/test/acceptance/albums_spec.js
@@ -3,9 +3,8 @@
 process.env.DBNAME = 'album-test';
 var app = require('../../app/app');
 var request = require('supertest');
-//var expect = require('chai').expect;
 var fs = require('fs');
-var rimraf = require('rimraf');
+var exec = require('child_process').exec;
 var Album;
 
 describe('albums', function(){
@@ -20,15 +19,50 @@ describe('albums', function(){
   });
 
   beforeEach(function(done){
-    var imgdir = __dirname + '/../../app/static/img';
-    rimraf.sync(imgdir);
-    fs.mkdirSync(imgdir);
-    var origfile = __dirname + '/../fixtures/euro.jpg';
-    var copyfile = __dirname + '/../fixtures/euro-copy.jpg';
-    fs.createReadStream(origfile).pipe(fs.createWriteStream(copyfile));
+    var testdir = __dirname + '/../../app/static/img/test*';
+    var cmd = 'rm -rf ' + testdir;
 
-    global.nss.db.dropDatabase(function(err, result){
-      done();
+    exec(cmd, function(){
+      var origfile = __dirname + '/../fixtures/euro.jpg';
+      var copy1file = __dirname + '/../fixtures/euro-copy1.jpg';
+      var copy2file = __dirname + '/../fixtures/euro-copy2.jpg';
+      fs.createReadStream(origfile).pipe(fs.createWriteStream(copy1file));
+      fs.createReadStream(origfile).pipe(fs.createWriteStream(copy2file));
+      global.nss.db.dropDatabase(function(err, result){
+        done();
+      });
+    });
+  });
+
+  describe('GET /', function(){
+    it('should display the album home page', function(done){
+      request(app)
+      .get('/')
+      .expect(200, done);
+    });
+  });
+
+  describe('GET /albums/3', function(){
+    var a1, a2, a3;
+
+    beforeEach(function(done){
+      a1 = new Album({title:'Test A', taken:'2012-03-25'});
+      a2 = new Album({title:'Test B', taken:'2012-03-26'});
+      a3 = new Album({title:'Test C', taken:'2012-03-27'});
+
+      a1.insert(function(){
+        a2.insert(function(){
+          a3.insert(function(){
+            done();
+          });
+        });
+      });
+    });
+
+    it('should display the album show page', function(done){
+      request(app)
+      .get('/albums/' + a1._id.toString())
+      .expect(200, done);
     });
   });
 
@@ -42,13 +76,33 @@ describe('albums', function(){
 
   describe('POST /albums', function(){
     it('should create a new album and send user back to home', function(done){
-      var filename = __dirname + '/../fixtures/euro-copy.jpg';
-
+      var filename = __dirname + '/../fixtures/euro-copy1.jpg';
       request(app)
       .post('/albums')
       .attach('cover', filename)
-      .field('title', 'European Vacation')
+      .field('title', 'Test Euro Vacation')
       .field('taken', '2014-02-25')
+      .expect(302, done);
+    });
+  });
+
+  describe('POST /albums/3', function(){
+    var a1;
+
+    beforeEach(function(done){
+      a1 = new Album({title:'Test A', taken:'2012-03-25'});
+      var oldname = __dirname + '/../fixtures/euro-copy1.jpg';
+      a1.addCover(oldname);
+      a1.insert(function(){
+        done();
+      });
+    });
+
+    it('should add a photo to the album', function(done){
+      var filename = __dirname + '/../fixtures/euro-copy2.jpg';
+      request(app)
+      .post('/albums/' + a1._id.toString())
+      .attach('photo', filename)
       .expect(302, done);
     });
   });
